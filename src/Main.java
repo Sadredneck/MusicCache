00dab16d2b7efd8af4d8879ff385051cf7f9f670
Sergey <gray4747@mail.ru>
2018-02-15 16:56:56 +0300

    Comm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("os.name"));
 
     }
 }
