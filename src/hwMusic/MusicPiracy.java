b602b671e16a701faea7228f62728f88b3b0184a
Sergey <gray4747@mail.ru>
2018-02-15 17:34:37 +0300

    Comm

--- src/hwMusic/MusicPiracy.java
+++ src/hwMusic/MusicPiracy.java
@@ -5,10 +5,16 @@ import java.nio.file.Paths;
 
 public class MusicPiracy {
     private static Path cachePath;
+
     static {
         switch (System.getProperty("os.name")) {
             case "Windows 7":
-                cachePath = Paths.get(System.getProperty("user.home"));
+                cachePath = Paths.get(System.getProperty("user.home"), "AppData", "Google", "Chrome", "User Data", "Default", "Media Cache");
+                System.out.println("wiwda");
+                break;
+            case "Mac Os X":
+                cachePath = Paths.get(System.getProperty("user.home"), "Caches", "Google", "Chrome", "Default", "Media Cache");
+                System.out.println("maca");
                 break;
         }
     }
