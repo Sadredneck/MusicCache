ce8b5972df009ecfee3d269d3f82586cd56cd974
Roman <Rolamanbuss@yandex.ru>
2018-02-15 19:34:18 +0300

    roman

--- src/hwMusic/MusicPiracy.java
+++ src/hwMusic/MusicPiracy.java
@@ -16,7 +16,7 @@ public class MusicPiracy {
                 cachePath = Paths.get(System.getProperty("user.home"), "AppData", "Google", "Chrome", "User Data", "Default", "Media Cache");
                 System.out.println("wiwda");
                 break;
-            case "Mac Os X":
+            case "Mac OS X":
                 cachePath = Paths.get(System.getProperty("user.home"), "Caches", "Google", "Chrome", "Default", "Media Cache");
                 System.out.println("maca");
                 break;
